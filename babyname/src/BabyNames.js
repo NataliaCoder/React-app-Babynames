c45f3fac5d319f91ef91b5c7281328afb381df94
Natalia <n5@mail.ru>
2020-09-04 20:22:06 +0100

    Completed level-3

--- babyname/src/BabyNames.js
+++ babyname/src/BabyNames.js
@@ -6,36 +6,86 @@ const BabyNames = (props) => {
   const sortedBabyNames = sortArray(props.names);
   const [namesToDisplay, setNamesToDisplay] = useState(sortedBabyNames);
   const [favouriteNames, setFavouriteNames] = useState([]);
+  const [gender, setGender] = useState("all");
 
   const type = (event) => {
     const inputValue = event.target.value;
-    const displayName = sortedBabyNames.filter((el) =>
-      el.name.toLowerCase().includes(inputValue.toLowerCase())
-    );
+    const displayName =
+      gender === "all"
+        ? sortedBabyNames.filter((el) =>
+            el.name.toLowerCase().includes(inputValue.toLowerCase())
+          )
+        : sortedBabyNames.filter(
+            (el) =>
+              el.name.toLowerCase().includes(inputValue.toLowerCase()) &&
+              el.sex === gender
+          );
     setNamesToDisplay(displayName);
   };
 
   const chooseName = (event) => {
-    console.log(event.target.innerText);
-    setFavouriteNames(favouriteNames.concat(event.target.innerText));
+    let clickedName = event.target.innerText;
+    const nameToAdd = namesToDisplay.find(
+      (names) => names.name === clickedName
+    );
+    // const updatedFavouriteNames = [...favouriteNames, nameToAdd];
+    const updatedFavouriteNames = favouriteNames.concat(nameToAdd);
+    setFavouriteNames(updatedFavouriteNames);
+
     setNamesToDisplay(
       namesToDisplay.filter((el) => el.name !== event.target.innerText)
     );
+  };
+  // reduce (filter+ map)
+  const unSelectName = (event) => {
+    let clickedFavName = event.target.id;
+    let clickedNameObject = favouriteNames.find(
+      (names) => names.name === clickedFavName
+    );
+    const deleteName = favouriteNames.filter(
+      (el) => el.name !== clickedFavName
+    );
+    setFavouriteNames(deleteName);
+    console.log(clickedFavName);
+    const addedClickedNameObject = sortArray([
+      ...namesToDisplay,
+      clickedNameObject,
+    ]);
+    setNamesToDisplay(addedClickedNameObject);
+  };
+  const handleGenderChoise = (event) => {
+    const genderFilterParametr = event.target.value;
+    setGender(genderFilterParametr);
+    const filteredNamesByGender =
+      genderFilterParametr === "all"
+        ? sortedBabyNames
+        : sortedBabyNames.filter((el) => el.sex === genderFilterParametr);
+    setNamesToDisplay(filteredNamesByGender);
   };
 
   return (
     <div>
       <div className="babyNames">
-        <p>Favs:</p>
-        {favouriteNames.map((el) => (
-          <p key={el}> {el} </p>
-        ))}
+        Favourites:
+        {favouriteNames.map((el) => {
+          let textColor = el.sex === "m" ? "boy" : "girl";
+          return (
+            <div
+              onClick={unSelectName}
+              id={el.name}
+              key={el.name}
+              className={textColor}
+            >
+              {el.name}
+            </div>
+          );
+        })}
       </div>
 
-      <Search type={type} />
+      <Search type={type} handleGenderChoise={handleGenderChoise} />
       <div className="babyNames">
         {namesToDisplay.map((el) => {
-          const textColor = el.sex === "m" ? "boy" : "girl";
+          let textColor = el.sex === "m" ? "boy" : "girl";
           return (
             <p onClick={chooseName} key={el.id} className={textColor}>
               {el.name}
--- babyname/src/Search.js
+++ babyname/src/Search.js
@@ -5,6 +5,12 @@ const Search = (props) => {
     <div>
       <label htmlFor="name">Find name:</label>
       <input className="www" id="name" onChange={props.type} type="text" />
+      <input onChange={props.handleGenderChoise} type="radio" id="male" name="gender" value="m" />
+      <label htmlFor="male">Male</label>
+      <input onChange={props.handleGenderChoise} type="radio" id="female" name="gender" value="f" />
+      <label htmlFor="female">Female</label>
+      <input onChange={props.handleGenderChoise} type="radio" id="all" name="gender" value="all" />
+      <label htmlFor="all">All names</label>
     </div>
   );
 };
