e192a0a2cf959c045f07b967628301f1e7064fc2
Natalia <n5@mail.ru>
2020-09-09 22:21:51 +0100

    saved changes

--- babyname/src/Search.js
+++ babyname/src/Search.js
@@ -6,7 +6,7 @@ const Search = (props) => {
       <label htmlFor="name">Find name:</label>
       <input className="www" id="name" onChange={props.type} type="text" />
       <input onChange={props.handleGenderChoise} type="radio" id="male" name="gender" value="m" />
-      <label htmlFor="male">Male</label>
+      <label htmlFor="male"><img src="sad_image.png" alt="I'm sad" /></label>
       <input onChange={props.handleGenderChoise} type="radio" id="female" name="gender" value="f" />
       <label htmlFor="female">Female</label>
       <input onChange={props.handleGenderChoise} type="radio" id="all" name="gender" value="all" />
