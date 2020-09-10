d36b042c605bb0287acfb002b26925389d434dc4
Natalia <n5@mail.ru>
2020-09-10 12:11:58 +0100

    Changed radio buttons

--- babyname/src/Search.js
+++ babyname/src/Search.js
@@ -4,13 +4,14 @@ const Search = (props) => {
   return (
     <div className="input_line">
       <label htmlFor="name">Find name: </label>
-      <input className="www" id="name" onChange={props.type} type="text" />
-      <input onChange={props.handleGenderChoise} type="radio" id="male" name="gender" value="m" />
-      <label htmlFor="male">Male</label>
-      <input onChange={props.handleGenderChoise} type="radio" id="female" name="gender" value="f" />
-      <label htmlFor="female">Female</label>
-      <input onChange={props.handleGenderChoise} type="radio" id="all" name="gender" value="all" />
-      <label htmlFor="all">All names</label>
+      <input className="input" id="name" onChange={props.type} type="text" />
+      <input onChange={props.handleGenderChoise} type="radio" id="male" name="gender" className="radio_button" value="m" />
+      <label htmlFor="male"><img width="50px" height="50px" border="2px solid black" text-align="center"
+ src="/male.png" alt="Male" /></label>
+      <input onChange={props.handleGenderChoise} type="radio" id="female" name="gender" className="radio_button" value="f" />
+      <label htmlFor="female"><img width="50px" height="50px" border="2px solid black" text-align="center" src="/female.png" alt="Female" /></label>
+      <input onChange={props.handleGenderChoise} type="radio" id="all" name="gender" value="all" defaultChecked />
+      <label htmlFor="all"><img width="50px" height="50px" border="2px solid black" text-align="center" src="/all.png" alt="All" /></label>
     </div>
   );
 };
