bb35dbba9eb551bcad4904cda0ea7ac7de59a6cf
Natalia <n5@mail.ru>
2020-09-10 13:27:23 +0100

    Changed CSS of buttons

--- babyname/src/Search.js
+++ babyname/src/Search.js
@@ -3,15 +3,21 @@ import React, { useState } from "react";
 const Search = (props) => {
   return (
     <div className="input_line">
-      <label htmlFor="name">Find name: </label>
+      <label htmlFor="name" className="input_title">Find name: </label>
       <input className="input" id="name" onChange={props.type} type="text" />
-      <input onChange={props.handleGenderChoise} type="radio" id="male" name="gender" className="radio_button" value="m" />
+      <div className="radio_btn">
+      <input onChange={props.handleGenderChoise} type="radio" id="male" name="gender" value="m" />
       <label htmlFor="male"><img width="50px" height="50px" border="2px solid black" text-align="center"
  src="/male.png" alt="Male" /></label>
-      <input onChange={props.handleGenderChoise} type="radio" id="female" name="gender" className="radio_button" value="f" />
+ </div>
+ <div className="radio_btn">
+      <input onChange={props.handleGenderChoise} type="radio" id="female" name="gender" value="f" />
       <label htmlFor="female"><img width="50px" height="50px" border="2px solid black" text-align="center" src="/female.png" alt="Female" /></label>
+      </div>
+      <div className="radio_btn">
       <input onChange={props.handleGenderChoise} type="radio" id="all" name="gender" value="all" defaultChecked />
       <label htmlFor="all"><img width="50px" height="50px" border="2px solid black" text-align="center" src="/all.png" alt="All" /></label>
+      </div>
     </div>
   );
 };
